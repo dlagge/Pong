3200283a4572d2d2f71099920afdb6bf988ceea3
Dominique <dominique.lagger@gmail.com>
2018-08-28 12:10:46 +0200

    Applet fertig

---
+++ src/PongV2/Tennis.java
@@ -0,0 +1,19 @@
+package PongV2;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Tennis extends Applet{
+
+	public void init(){
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+	
+	public void update(Graphics g) {
+		
+	}
+}
